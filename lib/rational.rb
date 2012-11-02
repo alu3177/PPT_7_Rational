6373d5982c33d6d5ea5fc726b560dc7fc389413b
Fernando González López-Peñalver <alu0100256543@ull.edu.es>
2012-11-02 19:53:57 +0000

    Segundo y Tercer test de Rspec completados.

--- lib/rational.rb
+++ lib/rational.rb
@@ -1,6 +1,27 @@
 class RationalNumber
-    attr_accessor :num
-    def initialize(numerador)
+    attr_accessor :num, :denom
+    def initialize(numerador, denominador)
         @num = numerador
+        @denom = denominador
+        reduce  # Simplificamos
     end
+
+    # Se encarga de simplificar la fraccion al minimo
+    def reduce
+        divisor = mcd
+        @num = @num / divisor
+        @denom = @denom / divisor
+    end
+
+    # Calcula el Máximo como un divisor (Algoritmo de Euclides)
+    def mcd
+        r = [@num, @denom]
+        i = 1
+        while r[i] != 0 do
+            r[i+1] = r[i-1] % r[i]
+            i = i + 1
+        end
+        return r[i-1]
+    end
+
 end
--- spec/rational_spec.rb
+++ spec/rational_spec.rb
@@ -2,14 +2,25 @@ require 'rational'
 
 describe RationalNumber do
 
-    let (:numerador) { 5 }
+    let (:numerador) { 6 }
+    let (:denominador) { 4 }
+    let (:reducida) {[3, 2]} # Array con el numerador y el denominador reducidos
 
     before :all do
-        @rat = RationalNumber.new(numerador)
+        @rat = RationalNumber.new(numerador, denominador)
     end
 
     it "Debe existir un numerador" do
         @rat.num.kind_of? Integer
+    end
+
+    it "Debe existir un denominador" do
+        @rat.denom.kind_of? Integer and @rat.denom > 0
+    end
+
+    it "Debe de estar en su forma reducida" do
+        @rat.num.should == reducida[0]
+        @rat.denom.should == reducida[1]
     end
 
 end
