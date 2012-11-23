c3833fbb841a856f6b8b1f444ea35e813f4e22a8
Fernando González López-Peñalver <alu0100256543@ull.edu.es>
2012-11-23 15:37:52 +0000

    Eliminados metodos innecesarios. Implementado coerce y las operaciones de multiplicacion, division y suma de RationalNumber con numeros enteros (Integer)

--- bin/rational_main.rb
+++ bin/rational_main.rb
@@ -1,11 +1,9 @@
 require 'rational'
 
-r1 = RationalNumber.new(6, 4)
+r1 = RationalNumber.new(3, 7)
 r2 = RationalNumber.new(12, 19)
 
 puts r1.mcd
-puts r1.mcd r2
-puts r1.mcm r2
 puts r1 - r2
 puts r1 * r2
 puts r1 / r2
@@ -14,3 +12,8 @@ puts r1 < r2
 puts r1 > r2
 puts r1 <= r2
 puts r1 >= r2
+puts r1 * 5
+puts r1 + 5
+puts 2 / r1
+puts r1 / 2
+puts r1 / 'b'
